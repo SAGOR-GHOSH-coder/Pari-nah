b430e590243dd158859284f30d099bee86694dc4
SAGOR-GHOSH-coder <sagor241-15-037@diu.edu.bd>
2025-05-23 19:53:03 +0600

    init commit

---
+++ src/ss.java
@@ -0,0 +1,14 @@
+public class ss{
+    public String id;
+    public float cgpa;
+    public String name;
+
+    public void doStudy() {
+        System.out.println("Study properly.");
+    }
+
+    public void display() {
+        System.out.println(id + " " + cgpa + " " + name);
+    }
+
+}
